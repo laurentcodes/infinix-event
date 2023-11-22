3882666e29f76db4d02fea8c9c9ba849e3183706
Saint Laurent <stlaurentcodes@gmail.com>
2023-11-22 10:32:56 +0100

    initial commit

---
+++ .next/server/pages/_document.js
@@ -0,0 +1,76 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/_document";
+exports.ids = ["pages/_document"];
+exports.modules = {
+
+/***/ "./src/pages/_document.js":
+/*!********************************!*\
+  !*** ./src/pages/_document.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Document)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var next_document__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/document */ \"./node_modules/next/document.js\");\n/* harmony import */ var next_document__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_document__WEBPACK_IMPORTED_MODULE_1__);\n\n\nfunction Document() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(next_document__WEBPACK_IMPORTED_MODULE_1__.Html, {\n        lang: \"en\",\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(next_document__WEBPACK_IMPORTED_MODULE_1__.Head, {\n                children: /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"title\", {\n                    children: \"Infinix - Event\"\n                }, void 0, false, {\n                    fileName: \"/Users/laurent/Desktop/infinix-event/src/pages/_document.js\",\n                    lineNumber: 7,\n                    columnNumber: 5\n                }, this)\n            }, void 0, false, {\n                fileName: \"/Users/laurent/Desktop/infinix-event/src/pages/_document.js\",\n                lineNumber: 6,\n                columnNumber: 4\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"body\", {\n                children: [\n                    /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(next_document__WEBPACK_IMPORTED_MODULE_1__.Main, {}, void 0, false, {\n                        fileName: \"/Users/laurent/Desktop/infinix-event/src/pages/_document.js\",\n                        lineNumber: 10,\n                        columnNumber: 5\n                    }, this),\n                    /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(next_document__WEBPACK_IMPORTED_MODULE_1__.NextScript, {}, void 0, false, {\n                        fileName: \"/Users/laurent/Desktop/infinix-event/src/pages/_document.js\",\n                        lineNumber: 11,\n                        columnNumber: 5\n                    }, this)\n                ]\n            }, void 0, true, {\n                fileName: \"/Users/laurent/Desktop/infinix-event/src/pages/_document.js\",\n                lineNumber: 9,\n                columnNumber: 4\n            }, this)\n        ]\n    }, void 0, true, {\n        fileName: \"/Users/laurent/Desktop/infinix-event/src/pages/_document.js\",\n        lineNumber: 5,\n        columnNumber: 3\n    }, this);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvcGFnZXMvX2RvY3VtZW50LmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7OztBQUE2RDtBQUU5QyxTQUFTSTtJQUN2QixxQkFDQyw4REFBQ0osK0NBQUlBO1FBQUNLLE1BQUs7OzBCQUNWLDhEQUFDSiwrQ0FBSUE7MEJBQ0osNEVBQUNLOzhCQUFNOzs7Ozs7Ozs7OzswQkFFUiw4REFBQ0M7O2tDQUNBLDhEQUFDTCwrQ0FBSUE7Ozs7O2tDQUNMLDhEQUFDQyxxREFBVUE7Ozs7Ozs7Ozs7Ozs7Ozs7O0FBSWYiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9pbmZpbml4LWV2ZW50Ly4vc3JjL3BhZ2VzL19kb2N1bWVudC5qcz9jNTA2Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IEh0bWwsIEhlYWQsIE1haW4sIE5leHRTY3JpcHQgfSBmcm9tICduZXh0L2RvY3VtZW50JztcblxuZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gRG9jdW1lbnQoKSB7XG5cdHJldHVybiAoXG5cdFx0PEh0bWwgbGFuZz0nZW4nPlxuXHRcdFx0PEhlYWQ+XG5cdFx0XHRcdDx0aXRsZT5JbmZpbml4IC0gRXZlbnQ8L3RpdGxlPlxuXHRcdFx0PC9IZWFkPlxuXHRcdFx0PGJvZHk+XG5cdFx0XHRcdDxNYWluIC8+XG5cdFx0XHRcdDxOZXh0U2NyaXB0IC8+XG5cdFx0XHQ8L2JvZHk+XG5cdFx0PC9IdG1sPlxuXHQpO1xufVxuIl0sIm5hbWVzIjpbIkh0bWwiLCJIZWFkIiwiTWFpbiIsIk5leHRTY3JpcHQiLCJEb2N1bWVudCIsImxhbmciLCJ0aXRsZSIsImJvZHkiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/pages/_document.js\n");
+
+/***/ }),
+
+/***/ "next/dist/compiled/next-server/pages.runtime.dev.js":
+/*!**********************************************************************!*\
+  !*** external "next/dist/compiled/next-server/pages.runtime.dev.js" ***!
+  \**********************************************************************/
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/next-server/pages.runtime.dev.js");
+
+/***/ }),
+
+/***/ "react":
+/*!************************!*\
+  !*** external "react" ***!
+  \************************/
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ }),
+
+/***/ "path":
+/*!***********************!*\
+  !*** external "path" ***!
+  \***********************/
+/***/ ((module) => {
+
+module.exports = require("path");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, ["vendor-chunks/next","vendor-chunks/@swc"], () => (__webpack_exec__("./src/pages/_document.js")));
+module.exports = __webpack_exports__;
+
+})();
---
+++ src/pages/index.jsx
@@ -0,0 +1,49 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+import logo from '../../public/assets/infinix-logo.png';
+
+export default function Home() {
+	return (
+		<main className='flex flex-col items-center h-screen bg-green-400'>
+			<Image alt='Infinix Logo' src={logo} width={200} />
+
+			<form className='flex flex-col gap-3 w-full md:w-1/4 items-center px-6 md:px-0 mt-12'>
+				<div className='flex flex-col gap-1 w-full'>
+					<label className='font-bold'>Social Network</label>
+					<select
+						className='p-2 border rounded-md focus:outline-none'
+						name='socials'
+					>
+						<option value='select'>Select Network</option>
+						<option value='x'>X</option>
+						<option value='facebook'>Facebook</option>
+						<option value='instagram'>Instagram</option>
+					</select>
+				</div>
+
+				<div className='flex flex-col gap-1 w-full'>
+					<label className='font-bold'>Name</label>
+					<input
+						className='p-2 border rounded-md focus:outline-none'
+						type='text'
+					/>
+				</div>
+
+				<div className='flex flex-col gap-1 w-full'>
+					<label className='font-bold'>Handle</label>
+					<input
+						className='p-2 border rounded-md focus:outline-none'
+						type='text'
+					/>
+				</div>
+
+				<button className='bg-white px-3 py-2 rounded-md mt-3 w-full hover:bg-green-800 hover:text-white transition-all duration-500'>
+					Submit
+				</button>
+			</form>
+		</main>
+	);
+}
